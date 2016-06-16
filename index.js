f797faefcfeae7d12bb9a3cc1ac4bef604b869e8
nepynepp <choice.nnn@gmail.com>
2016-06-16 15:42:08 +0700

    learn array

--- index.js
+++ index.js
@@ -4,5 +4,20 @@ const  student = {
     name: 'nep'
 };
 
-console.log('id:',student.id);
-console.log('name:',student.name);
+const  student1 = {
+    id: '562115037',
+    name: 'que'
+};
+
+const  student2 = {
+    id: '562115038',
+    name: 'toey'
+};
+
+const  student3 = {
+    id: '562115039',
+    name: 'who'
+};
+const students = [student,student2,student3]
+students.splice(1,1);
+console.log(students);
