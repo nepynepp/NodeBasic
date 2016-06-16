66b72da3f150cf8dc9798a2cb068087b7118693b
nepynepp <choice.nnn@gmail.com>
2016-06-16 17:29:28 +0700

    learn higher-order function

--- index.js
+++ index.js
@@ -1,31 +1,37 @@
 'use strict';
-const  student = {
-    id: '562115036',
-    name: 'nep'
-};
 
 const  student1 = {
-    id: '562115037',
-    name: 'que'
+    id: '562115036',
+    name: 'nep',
+    grade: '4.00'
 };
 
 const  student2 = {
     id: '562115038',
-    name: 'toey'
+    name: 'toey',
+    grade: '1.98'
 };
 
 const  student3 = {
     id: '562115039',
-    name: 'who'
+    name: 'who',
+    grade: '2.45'
 };
-const students = [student,student2,student3]
-const getstudentById = (id,name) => {
- switch (id){
-     case '562115036':
-         return 'good';
-     case '562115037':
-         return'bad';
- }
+const students = [student1,student2,student3]
+
+const getStudentById = (id) => {
+    console.log(students
+        .filter(student => student.id == id)
+        .map(student => 'This is student number : ' + student.id)
+        .pop());
 };
 
-console.log(getstudentById('562115036'));
+const isSomeStudentHasGradeGreaterThan2 = () => {
+    return students.some(student => student.grade > 2.0);
+}
+//getStudentById('562115038');
+//console.log(isSomeStudentHasGradeGreaterThan2());
+
+const  numbers = [1,2,3];
+const  sum = numbers.reduce((total,nextValue) => total+nextValue,10);
+console.log(sum);
